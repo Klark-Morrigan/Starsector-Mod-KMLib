ff41946f467d49698ba4a5b8a508cfaa2b80f76f
Klark Morrigan <coverthelirush@gmail.com>
2026-09-10 17:10:45 +0700

    chore: remove unused import

--- src/buildScriptTest/java/kmlib/gradlescripts/integrationtests/ReportKmlibVersionMismatchIntegrationTests.java
+++ src/buildScriptTest/java/kmlib/gradlescripts/integrationtests/ReportKmlibVersionMismatchIntegrationTests.java
@@ -1,6 +1,5 @@
 package kmlib.gradlescripts.integrationtests;
 
-import org.gradle.testkit.runner.BuildResult;
 import org.gradle.testkit.runner.GradleRunner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
